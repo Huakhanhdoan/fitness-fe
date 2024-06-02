77a0a85da066e3a120c325706ad46cb83e6bd608
Nguyễn Đức Anh <datnguyen1854@gmail.com>
2024-06-02 13:15:42 +0700

    guide

---
+++ lib/setting/guide.dart
@@ -0,0 +1,19 @@
+import 'package:fitness/untils/color.dart';
+import 'package:flutter/material.dart';
+
+class Guide extends StatefulBuilder {
+  const Guide({super.key, required super.builder});
+
+  @override
+  _GuideState createState() =>_GuideState();
+}
+
+class _GuideState extends State<Guide> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+}
